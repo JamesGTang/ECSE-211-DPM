4b941f75612b58a93ec4ab75ff470079da3f3f28
James <james.tang@nodetech.co>
2018-03-20 00:38:41 -0400

    Commit

--- Final/src/ca/mcgill/ecse211/lightsensor/LightSensorController.java
+++
@@ -1,6 +0,0 @@
-package ca.mcgill.ecse211.lightsensor;
-
-public interface LightSensorController {
-	public void processLightData(int tb);
-	public int readLightData();
-}
--- Final/src/ca/mcgill/ecse211/ultrasonic/UltrasonicController.java
+++
@@ -1,8 +0,0 @@
-package ca.mcgill.ecse211.ultrasonic;
-
-public interface UltrasonicController {
-
-  public void processUSData(double distance);
-
-  public double readUSDistance();
-}
